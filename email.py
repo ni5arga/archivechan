9d652c88777c5ed4af84f51ae0130c5a9f3efaa2
Nisarga <git@nisarga.me>
2025-04-01 23:39:20 +0530

    email.py

--- email.py
+++ email.py
@@ -1,7 +1,17 @@
-import re
-import requests
+import imaplib
+import email
 
-def extract_emails_from_url(url):
-    response = requests.get(url)
-    emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', response.text)
-    return list(set(emails))
+def save_emails(username, password, output_dir):
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login(username, password)
+    mail.select('inbox')
+    result, data = mail.search(None, 'ALL')
+    email_ids = data[0].split()
+    os.makedirs(output_dir, exist_ok=True)
+    for e_id in email_ids:
+        result, data = mail.fetch(e_id, '(RFC822)')
+        raw_email = data[0][1]
+        email_message = email.message_from_bytes(raw_email)
+        filename = f"email_{e_id.decode()}.eml"
+        with open(os.path.join(output_dir, filename), 'wb') as f:
+            f.write(raw_email)
